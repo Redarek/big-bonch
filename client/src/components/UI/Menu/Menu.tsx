5bbc00c0b99bb1cae1f7b2ed835fa7f1c452ec6f
iiVladii <vladislav2407@icloud.com>
2022-11-23 22:40:10 +0300

    reg login fix menu logout btn

--- client/src/components/UI/Menu/Menu.tsx
+++ client/src/components/UI/Menu/Menu.tsx
@@ -5,6 +5,7 @@ import Quests from "./Quests/Quests";
 import {useAppDispatch, useAppSelector} from "../../../hooks/redux";
 import {setIsShow, setTitle} from "../../../store/reducers/inventorySlice";
 import Notification from "../Notification/Notification";
+import {logout} from "../../../store/reducers/ActionCreators";
 
 interface MenuProps {
     // setIsShowMenu: (bol: boolean) => void;
@@ -20,14 +21,14 @@ interface Reward {
 const Menu: FC<MenuProps> = ({menuIndex}) => {
     const [notificationMintNFTIsVisible, setNotificationMintNFTIsVisible] = useState<boolean>(false)
     const [notificationItem, setNotificationItem] = useState<Reward>({} as Reward)
-    const menuWindows = [<Inventory setNotificationItem={setNotificationItem} setNotificationMintNFTIsVisible={setNotificationMintNFTIsVisible}/>, <Quests/>]
+    const menuWindows = [<Inventory setNotificationItem={setNotificationItem} setNotificationMintNFTIsVisible={setNotificationMintNFTIsVisible}/>, <div>NFT</div>, <Quests/>]
     const dispatch = useAppDispatch()
     const title = () => {
         switch (menuIndex) {
-            case 0:
-                return 'Инвентарь';
-            case 1:
-                return 'Задания'
+            case 0:return 'Инвентарь';
+            case 1:return 'NFT'
+            case 2:return 'Квесты'
+            // case 3:return 'Настройки'
         }
     }
     return (
@@ -46,8 +47,11 @@ const Menu: FC<MenuProps> = ({menuIndex}) => {
                     <div className={cl.menuNavWrap}>
                         <div className={cl.itemTitle}>Прогресс:</div>
                         <div className={cl.menuNavItem} onClick={() => dispatch(setTitle(0))}>Инвентарь</div>
+                        <div className={cl.menuNavItem} onClick={() => dispatch(setTitle(1))}>NFT</div>
                         <div className={cl.itemTitle}>Задания:</div>
-                        <div className={cl.menuNavItem} onClick={() => dispatch(setTitle(1))}>Квесты</div>
+                        <div className={cl.menuNavItem} onClick={() => dispatch(setTitle(2))}>Квесты</div>
+                        <div className={cl.itemTitle}>Настройки:</div>
+                        <div className={cl.menuNavItem} onClick={() => dispatch(logout())}>Покинуть игру</div>
                     </div>
                     <div className={cl.menuWindow}>
                         {menuWindows[menuIndex]}
