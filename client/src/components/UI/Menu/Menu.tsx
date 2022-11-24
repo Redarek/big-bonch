973aff03bd3f0067f138789db971e265d70578a2
iiVladii <vladislav2407@icloud.com>
2022-11-24 21:32:57 +0300

    borders

--- client/src/components/UI/Menu/Menu.tsx
+++ client/src/components/UI/Menu/Menu.tsx
@@ -21,13 +21,18 @@ interface Reward {
 const Menu: FC<MenuProps> = ({menuIndex}) => {
     const [notificationMintNFTIsVisible, setNotificationMintNFTIsVisible] = useState<boolean>(false)
     const [notificationItem, setNotificationItem] = useState<Reward>({} as Reward)
-    const menuWindows = [<Inventory setNotificationItem={setNotificationItem} setNotificationMintNFTIsVisible={setNotificationMintNFTIsVisible}/>, <div>NFT</div>, <Quests/>]
+    const menuWindows = [<Inventory setNotificationItem={setNotificationItem}
+                                    setNotificationMintNFTIsVisible={setNotificationMintNFTIsVisible}/>, <div>NFT</div>,
+        <Quests/>]
     const dispatch = useAppDispatch()
     const title = () => {
         switch (menuIndex) {
-            case 0:return 'Инвентарь';
-            case 1:return 'NFT'
-            case 2:return 'Квесты'
+            case 0:
+                return 'Инвентарь';
+            case 1:
+                return 'NFT'
+            case 2:
+                return 'Квесты'
             // case 3:return 'Настройки'
         }
     }
@@ -45,13 +50,19 @@ const Menu: FC<MenuProps> = ({menuIndex}) => {
                 </div>
                 <div className={cl.menuContent}>
                     <div className={cl.menuNavWrap}>
-                        <div className={cl.itemTitle}>Прогресс:</div>
-                        <div className={cl.menuNavItem} onClick={() => dispatch(setTitle(0))}>Инвентарь</div>
-                        <div className={cl.menuNavItem} onClick={() => dispatch(setTitle(1))}>NFT</div>
-                        <div className={cl.itemTitle}>Задания:</div>
-                        <div className={cl.menuNavItem} onClick={() => dispatch(setTitle(2))}>Квесты</div>
-                        <div className={cl.itemTitle}>Настройки:</div>
-                        <div className={cl.menuNavItem} onClick={() => dispatch(logout())}>Покинуть игру</div>
+                        <div className={cl.menuNav}>
+                            <div className={cl.itemTitle}>Прогресс:</div>
+                            <div className={cl.menuNavItem} onClick={() => dispatch(setTitle(0))}>Инвентарь</div>
+                            <div className={cl.menuNavItem} onClick={() => dispatch(setTitle(1))}>NFT</div>
+                        </div>
+                        <div className={cl.menuNav}>
+                            <div className={cl.itemTitle}>Задания:</div>
+                            <div className={cl.menuNavItem} onClick={() => dispatch(setTitle(2))}>Квесты</div>
+                        </div>
+                        <div className={cl.menuNav}>
+                            <div className={cl.itemTitle}>Настройки:</div>
+                            <div className={cl.menuNavItem} onClick={() => dispatch(logout())}>Покинуть игру</div>
+                        </div>
                     </div>
                     <div className={cl.menuWindow}>
                         {menuWindows[menuIndex]}
